16a13884c0dba6f804cd02445f614b94deadc351
Chris Hamons <chris.hamons@xamarin.com>
2018-04-10 15:19:03 -0500

    Add simple library and ignore

---
+++ SimpleLib/CSharp/SimpleLib/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SimpleLib
+{
+	public class Adder
+	{
+		public int X { get; }
+		public int Y { get; }
+		
+		public Adder (int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public int Value () => X + Y;
+	}
+}
